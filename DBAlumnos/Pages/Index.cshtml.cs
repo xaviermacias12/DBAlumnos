e5440f0029b170b9b86045c7dc2a810a971b4de7
luis xavier macias flores <luisxaviermaciasflores@outlook.com>
2026-03-19 11:41:18 -0600

    Ahora Index muestra la lista de alumnos (antes era Consulta)

--- DBAlumnos/Pages/Index.cshtml.cs
+++ DBAlumnos/Pages/Index.cshtml.cs
@@ -1,13 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using DBAlumnos.Data;
+using DBAlumnos.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DBAlumnos.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _context;
+        public List<Alumno> ListaAlumnos { get; set; }
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
-
+            ListaAlumnos = _context.Alumnos.ToList();
         }
     }
 }
